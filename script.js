a253985e5e9184c69a98669d93d220179b69711c
DragonHubRBX <ahmedrawi345@gmail.com>
2024-08-07 12:50:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
 $(document).ready(function() {
-   $("#mugen").hide().show(3000);
-   $("#text_box").css('background-color', 'black');
-   var hold = $("#text_box").val();
-   // alert(hold);   
+    // Show the #mugen form with a fade-in effect
+    $("#mugen").hide().fadeIn(3000);
+    
+    // Set the background color of #text_box
+    $("#text_box").css('background-color', 'black');
 });
 
 $(function() {
-   var list = [
-      "Option2",
-      "Dragons"
-   ];
-   $("#text_box").autocomplete({
-      source: list
-   });
+    // Autocomplete feature for #text_box
+    var list = ["Option2", "Dragons"];
+    $("#text_box").autocomplete({
+        source: list
+    });
 });
 
-function populate(s1, s2) {
-   var s1 = document.getElementById(s1);
-   var s2 = document.getElementById(s2);
-   s2.innerHTML = "";
-   if (s1.value == "op2") {
-      var optionArray = ["default|Choose Test", "test1|Test1", "test2|Test2", "test3|Test3"]
-   }
-   if (s1.value == "op3") {
-      var optionArray = ["default|Choose Dragon", "dg1|Dragos", "dg2|Yggdragsil", "dg3|Kuzuryu"]
-   }
-   for (var option in optionArray) {
-      var pair = optionArray[option].split("|");
-      var newOption = document.createElement("option");
-      newOption.value = pair[0];
-      newOption.innerHTML = pair[1];
-      s2.options.add(newOption);
-   }
-
-   function change(s3, s4) {
-      var s3 = document.getElementById(s3);
-      var s4 = document.getElementById(s4);
-      s4.innerHTML = "";
-
-      if (s2.value == "default") {
-         var box = "Dragons";
-         var newInput = document.createElement("option");
-         newInput.value = box;
-         alert(newInput);
-         s4.options.add(newInput);
-      }
-      s4.options.add(newInput);
-
-   }
-}
-
-var t1 = document.getElementById("time");
-var t2 = new Date("October 13, 2014");
-t1.innerHTML = t2;
+// Improved Date Display
+$(function() {
+    var t1 = document.getElementById("time");
+    var currentDate = new Date();
+    
+    // Options for formatting the date
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    t1.innerHTML = currentDate.toLocaleDateString("en-US", options);
+});
