058e0e5484cb027dd096547995064710c3161a72
DragonHubRBX <ahmedrawi345@gmail.com>
2024-08-07 12:59:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 $(document).ready(function() {
-    // Show the #mugen form with a fade-in effect
-    $("#mugen").hide().fadeIn(3000);
-    
-    // Set the background color of #text_box
-    $("#text_box").css('background-color', 'black');
-});
+    $("#main-content").hide().fadeIn(2000);
 
-$(function() {
-    // Autocomplete feature for #text_box
-    var list = ["Option2", "Dragons"];
-    $("#text_box").autocomplete({
-        source: list
-    });
-});
-
-// Improved Date Display
-$(function() {
-    var t1 = document.getElementById("time");
-    var currentDate = new Date();
-    
-    // Options for formatting the date
+    // Setting the footer date dynamically
+    var today = new Date();
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    t1.innerHTML = currentDate.toLocaleDateString("en-US", options);
+    var dateString = today.toLocaleDateString("en-US", options);
+    $("#footer p").text(dateString);
 });
